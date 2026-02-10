b389a3f54c7de3f0a70e896c56a8cfd2eee16122
Joessuf <yousefbakkar82@gmail.com>
2026-02-10 21:20:22 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -25,12 +25,20 @@ while(1) // REPL (Read-Eval-Print Loop) is an interactive loop that forms the co
   input[strlen(input) - 1] = '\0';
 
 
-  if(strcmp(input + 5, "exit") == 0) printf("exit is a shell builtin\n");
-  else if(strcmp(input + 5, "echo") == 0) printf("echo is a shell builtin\n");
-  else if(strcmp(input + 5, "type") == 0) printf("type is a shell builtin\n");
+  if(strcmp(input, "exit") == 0) exit(0); // isnt input here a char how did we compare it to a string ??
+
+  else if (strncmp(input, "echo", 3) == 0) printf("%s\n", input + 5);
+
+  else if (strncmp(input, "type", 3) == 0)
+  {
+      if(strncmp(input + 3, "type", 3) == 0) printf("%s is a shell builtin\n", input + 4);
+      else if(strncmp(input + 3, "echo", 3) == 0) printf("%s is a shell builtin\n", input + 4);
+      else if(strncmp(input + 3, "exit", 3) == 0) printf("%s is a shell builtin\n", input + 4);
+      else printf("%s invalid_command: not found", input);
+  }
 
   else
-  printf("%s: not found\n", input + 5);
+  printf("%s: command not found\n", input);
 
 }
 
